1d89b6601ffdde3703c6d95c81f0849ac01204a1
Nicholas Lauria <nicolauria@Nicholass-MacBook-Air.local>
2021-05-24 09:15:34 -0700

    add support for env variables

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::GamesController < ApplicationController
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
         request = Net::HTTP::Get.new(url)
-        request["x-rapidapi-key"] = '9a4d099c92msh2a1c415f181c7a5p1dafe6jsn3a30d68ca532'
+        request["x-rapidapi-key"] = ENV["X-RAPIDAPI-KEY"]
         request["x-rapidapi-host"] = 'sportspage-feeds.p.rapidapi.com'
 
         response = http.request(request)
@@ -28,7 +28,7 @@ class Api::V1::GamesController < ApplicationController
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
         request = Net::HTTP::Get.new(url)
-        request["x-rapidapi-key"] = '9a4d099c92msh2a1c415f181c7a5p1dafe6jsn3a30d68ca532'
+        request["x-rapidapi-key"] = ENV["X-RAPIDAPI-KEY"]
         request["x-rapidapi-host"] = 'sportspage-feeds.p.rapidapi.com'
 
         response = http.request(request)
