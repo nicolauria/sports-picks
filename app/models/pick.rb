ad30bc23c50b897eb8402d976dfbea9a7b776766
Nicholas Lauria <nicolauria@Nicholass-MacBook-Air.local>
2021-05-24 18:18:32 -0700

    add pick model validations

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -1,2 +1,9 @@
 class Pick < ApplicationRecord
+    validates :game_id, presence: true
+    validates :user_email, presence: true
+    validates :home_or_away, presence: true
+    validates :summary, presence: true
+    validates :league, presence: true
+    validates :home_odds, presence: true
+    validates :away_odds, presence: true
 end
